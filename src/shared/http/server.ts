e64eca7026161df797352b35967103f3fb420166
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-10-14 23:08:52 -0300

    adds TypeORM

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,25 +1,15 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 import { routes } from './routes';
-import { AppError } from '@shared/errors/AppError';
+import { AppDataSource } from './data-source';
+import { errorMiddleware } from './middlewares/error';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use(routes);
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res
-      .status(error.statuscode)
-      .json({ status: 'error', message: error.message });
-  }
-
-  return res
-    .status(500)
-    .json({ status: 'error', message: 'Internal Server Error' });
+AppDataSource.initialize().then(() => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+  app.use(errorMiddleware);
+  app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
 });
-
-app.listen(3333, () => console.log('Server started on port 3333!'));
