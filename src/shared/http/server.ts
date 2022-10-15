739563200a692f3363c6d93a8f22ffb2cb436e83
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-10-15 11:21:20 -0300

    Adds environment variable

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -11,5 +11,7 @@ AppDataSource.initialize().then(() => {
   app.use(express.json());
   app.use(routes);
   app.use(errorMiddleware);
-  app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
+  app.listen(process.env.PORT, () =>
+    console.log('🚀 Server started on port 3333!'),
+  );
 });
