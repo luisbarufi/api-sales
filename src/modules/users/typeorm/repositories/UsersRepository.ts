7f823dbd02fd4f6b0aff8489a494958bffe650d2
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-10-20 18:45:21 -0300

    Adds UsersRepository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,31 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../../../../shared/typeorm/data-source';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { User } from '../entities/User';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async create({
+    id,
+    name,
+    email,
+    password,
+    avatar,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      id,
+      name,
+      email,
+      password,
+      avatar,
+    });
+
+    await this.repository.save(user);
+  }
+}
